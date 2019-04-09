5aed32dd0e0e94f6681e7cde709de9f980b279a8
Harsh Srivatava <srivastavaharsh9888@gmail.com>
2019-04-10 03:49:20 +0530

    Update customerService.service.ts

--- frontend/customerCare/src/app/services/customerService/customerService.service.ts
+++ frontend/customerCare/src/app/services/customerService/customerService.service.ts
@@ -11,7 +11,7 @@ export class CustomerService {
   getCompanyState(): Subject<string> {
     const dataSubject = new Subject<any>();
     this.http.get(
-      `http://18.224.150.95/list/`)
+      `http://server.customerhelp.tk/list/`)
       .subscribe((data) => {
         dataSubject.next(data);
       },(err) => {
@@ -24,7 +24,7 @@ export class CustomerService {
     const dataSubject = new Subject<any>();
     let id: number;
     this.http.get(
-      `http://18.224.150.95/message/?msgId=${msgId}&companyId=${companyId}`)
+      `http://server.customerhelp.tk/message/?msgId=${msgId}&companyId=${companyId}`)
       .subscribe((message) => {
         dataSubject.next(message);
       },(err)=>{
